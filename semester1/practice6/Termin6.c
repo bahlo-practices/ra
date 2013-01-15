43eef8c8e6b386e53cf9b2e9699879c5e5eb73d4
Arne Bahlo <hallo@arne.me>
2013-01-15 18:16:07 +0100

    added practice 6

---
+++ semester1/practice6/Termin6.c
@@ -0,0 +1,25 @@
+// C-Programm um die in Assembler geschriebene Funktion zu testen.
+// Das Programm kann/sollte für weitere Tests erweitert werden.
+// von: Manfred
+// vom: 16.02.2011 
+int searchStringInString(char*, char*);
+
+int main (void)
+{
+	int Anzahl;
+	char* Text="hallo Alles klar?";
+	char* Suchstring="ll";
+	char* Leerstring="";
+	
+	// Custom
+	Anzahl = searchStringInString("aaaaa", "aa");
+	
+	Anzahl = searchStringInString(Text, Suchstring);
+	Anzahl = searchStringInString(Text, Leerstring);	
+	Anzahl = searchStringInString(Leerstring, Suchstring);
+	Anzahl = searchStringInString(Leerstring, Leerstring);
+// Aufrufe funktionieren auch so..
+	Anzahl = searchStringInString("aabaabaab", "ab");
+	
+	return 0;
+}
